065cbe1ece1b64ee270ed139208998dbf566e55a
Enzo Garbi <enzo@example.com>
2026-03-04 14:42:10 -0300

    Mover API Key SendGrid a variable de entorno SENDGRID_API_KEY

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,7 +141,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY', '')
 DEFAULT_FROM_EMAIL = 'graficamelfa@gmail.com'
 
 # Auth Settings
