d90dbadf3a9c64b897e53d5615fa9272dc883f4a
Enzo Garbi <enzo@example.com>
2026-05-05 13:45:04 -0300

    Fix: usar StaticFilesStorage estandar de Django

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
     },
 }
 
